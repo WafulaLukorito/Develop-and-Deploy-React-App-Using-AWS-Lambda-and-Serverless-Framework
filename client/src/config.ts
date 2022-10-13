4e4fce24de7e3862b6be317721a115a2061bd152
lukorito <jonesdeelder@gmail.com>
2022-10-13 23:42:45 +0300

    first commit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'n5kiust7z5';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-boqwmfvy.us.auth0.com',
+  clientId: 'Xxjwd6E5O1CbbLerbq7YuNGFkERGXuiQ',
+  callbackUrl: 'http://localhost:3000/callback'
+};
